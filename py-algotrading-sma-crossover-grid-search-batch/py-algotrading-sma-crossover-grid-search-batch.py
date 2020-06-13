ed9650d249994b30112b4a24a8123229b495aef4
David Lowe <david.lowe@dainesanalytics.com>
2020-06-13 13:33:01 -0700

    Upload #1 of simple moving average crossover grid search trading model batch mode

---
+++ py-algotrading-sma-crossover-grid-search-batch/py-algotrading-sma-crossover-grid-search-batch.py
@@ -0,0 +1,383 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Algorithmic Trading Model for Simple Moving Average Crossover Grid Search Batch Mode
+# ### David Lowe
+# ### June 17, 2020
+# 
+# NOTE: This script is for learning purposes only and does not constitute a recommendation for buying or selling any stock mentioned in this script.
+# 
+# SUMMARY: The purpose of this project is to construct and test an algorithmic trading model and document the end-to-end steps using a template.
+# 
+# INTRODUCTION: This algorithmic trading model examines a series of simple moving average (MA) models via a grid search methodology. When the fast moving-average curve crosses above the slow moving-average curve, the strategy goes long (buys) on the stock. When the opposite occurs, we will exit the position.
+# 
+# The grid search methodology will search through all combinations between the two MA curves. The faster MA curve can range from 5 days to 20 days, while the slower MA can range from 10 days to 50 days. Both curves use a 5-day increment. We will apply the analysis on the GOOG stock for the trading period between 2019-1-1 to 2020-06-15.
+# 
+# Dataset ML Model: Time series analysis with numerical attributes
+# 
+# Dataset Used: Quandl
+# 
+# An algorithmic trading modeling project generally can be broken down into about five major tasks:
+# 
+# 1. Prepare Environment
+# 2. Acquire and Pre-Process Data
+# 3. Develop Strategy and Train Model
+# 4. Back-test Model
+# 5. Evaluate Performance
+
+# ## Task 1. Prepare Environment
+
+# In[1]:
+
+
+import os
+import sys
+import smtplib
+import numpy as np
+import pandas as pd
+import requests
+import json
+from email.message import EmailMessage
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+
+# In[2]:
+
+if len(sys.argv) < 3 :
+    sys.exit("Not sufficient parameters supplied. Script Processing Aborted!!!")
+else:
+    print('Processing the ticker symbol:', sys.argv[1])
+
+# Set up the redirection of output to a file
+orig_stdout = sys.stdout
+filename = "algotrading-sma-crossover_" + datetime.now().strftime('%Y%m%d') + "_" + sys.argv[1] + ".txt"
+f = open(filename, 'w')
+sys.stdout = f
+
+print('Processing the ticker symbol:', sys.argv[1])
+
+
+# In[3]:
+
+
+# Begin the timer for the script processing
+startTimeScript = datetime.now()
+
+# Set up the verbose flag to print detailed messages for debugging (setting True will activate!)
+if sys.argv[3] == 'verbose' :
+    verbose = True
+else:
+    verbose = False
+
+# Set up the sendNotification flag to send progress emails (setting True will send emails!)
+notifyStatus = False
+
+# Set Pandas options
+pd.set_option("display.max_rows", 120)
+pd.set_option("display.width", 140)
+
+
+# In[4]:
+
+
+if notifyStatus: email_notify("Task 1. Prepare Environment has begun! "+datetime.now().strftime('%a %B %d, %Y %I:%M:%S %p'))
+
+
+# In[5]:
+
+
+stock_symbol = sys.argv[1]
+initial_capital = 0
+
+# Specify the parameters for the trading strategy
+fast_ma_min = 5
+fast_ma_max = 20
+slow_ma_min = 10
+slow_ma_max = 50
+ma_increment = 5
+min_ma_gap = 5
+
+# model_start_date = datetime(2019, 1, 1)
+model_start_date = datetime.strptime(sys.argv[2], '%Y-%m-%d')
+print("Starting date for the model:", model_start_date)
+stock_start_date = model_start_date - timedelta(days=int(slow_ma_max*1.5)) # Need more pricing data to calculate moving averages
+
+# model_end_date = datetime(2020, 1, 1)
+model_end_date = datetime.now()
+print("Ending date for the model:", model_end_date)
+print()
+
+
+# ## Task 2. Acquire and Pre-Process Data
+
+# In[8]:
+
+
+# Check and see whether the API key is available
+quandl_key = os.environ.get('QUANDL_API')
+if quandl_key is None: sys.exit("API key for Quandl not available. Script Processing Aborted!!!")
+
+
+# In[9]:
+
+
+start_date_string = stock_start_date.strftime('%Y-%m-%d')
+end_date_string = model_end_date.strftime('%Y-%m-%d')
+quandl_url = "https://www.quandl.com/api/v3/datatables/SHARADAR/SEP.json?date.gte=%s&date.lte=%s&ticker=%s&api_key=%s" % (start_date_string, end_date_string, stock_symbol, quandl_key)
+print('Fetching equity data from', start_date_string, 'to', end_date_string)
+response = requests.get(quandl_url)
+quandl_dict = json.loads(response.text)
+stock_quandl = pd.DataFrame(quandl_dict['datatable']['data'])
+print(len(stock_quandl), 'data points retrieved from the API call.')
+if verbose:
+    print(quandl_dict)
+    print()
+
+
+# In[11]:
+
+
+stock_quandl.columns = ['ticker', 'date', 'open', 'high', 'low', 'close', 'volume', 'dividend', 'closeunadj', 'lastupdated']
+# stock_quandl.set_index('date', inplace=True)
+stock_quandl.index = pd.to_datetime(stock_quandl.date)
+stock_quandl = stock_quandl.sort_index(ascending = True)
+if verbose:
+    print(stock_quandl.info(verbose=True))
+    print()
+
+
+# ## Task 3. Develop Strategy and Train Model
+
+# ### 3.a) Set up the Dataframe for the Trading Model
+
+# In[17]:
+
+
+# Select the data source and pricing columns to use for modeling
+model_template = stock_quandl.loc[:, ['open','close']]
+
+# Set up the standard column name for modeling
+model_template.rename(columns={'open': 'open_price', 'close': 'close_price'}, inplace=True)
+if verbose: model_template.info(verbose=True)
+
+
+# ### 3.b) Set up the Analysis Table with Indicators
+
+# In[20]:
+
+
+def trading_ma_crossover(model):
+    waitfor_first_entry = True
+    for x in range(len(model)):
+        if model['ma_change'].iloc[x] > 0:
+            model['trade_signal'].iloc[x] = 1  # trade_signal = 1 means we should take a long position
+        else:
+            model['trade_signal'].iloc[x] = 0  # trade_signal = 0 means we should take a flat position
+        if x != 0:
+            model['signal_change'].iloc[x] = model['trade_signal'].iloc[x] - model['trade_signal'].iloc[x-1]
+            if waitfor_first_entry and (model['signal_change'].iloc[x-1] == 1):
+                model['entry_exit'].iloc[x] = model['signal_change'].iloc[x-1]
+                waitfor_first_entry = False
+            elif (not waitfor_first_entry) and (model['signal_change'].iloc[x-1] != 0):
+                model['entry_exit'].iloc[x] = model['signal_change'].iloc[x-1]
+#     model['entry_exit'].iloc[-1] = -1  # the model assumes we will always exit at tht end of the modeling period
+
+
+# In[21]:
+
+
+model_collection = {}
+serial_number = 1
+
+for slow_ma in range(slow_ma_min, slow_ma_max+1, ma_increment):
+    for fast_ma in range(fast_ma_min, fast_ma_max+1, ma_increment):
+        if (slow_ma - fast_ma) < min_ma_gap: break
+        model_name = 'SMA_' + str(serial_number).zfill(3) + '_SlowMA_' + str(slow_ma).zfill(2) + '_FastMA_' + str(fast_ma).zfill(2)
+        serial_number = serial_number + 1
+        trading_model = model_template.copy()
+        trading_model['fast_ma'] = trading_model['close_price'].rolling(fast_ma).mean()
+        trading_model['slow_ma'] = trading_model['close_price'].rolling(slow_ma).mean()
+        trading_model['ma_change'] = trading_model['fast_ma'] - trading_model['slow_ma']
+        trading_model['trade_signal'] = np.zeros(len(trading_model))
+        trading_model['signal_change'] = np.zeros(len(trading_model))
+        trading_model['entry_exit'] = np.zeros(len(trading_model))
+        trading_model = trading_model[model_start_date:model_end_date]
+        trading_ma_crossover(trading_model)
+        model_collection[model_name] = trading_model.copy()
+
+print(serial_number, 'models added to the trading model collection.')
+print()
+
+
+# In[22]:
+
+
+# List the entry/exit points for each model
+for key in model_collection:
+    if verbose:
+        print('List the signal change and entry/exit points for', key)
+        print(model_collection[key][(model_collection[key].signal_change != 0) | (model_collection[key].entry_exit != 0)])
+        print()
+
+
+# ## Task 4. Back-test Model
+
+# In[26]:
+
+
+def trading_portfolio_generation(initial_fund, trading_model):
+    # Construct a portfolio to track the transactions and returns
+    portfolio = pd.DataFrame(index=trading_model.index, columns=['trade_action', 'qty_onhand', 'cost_basis', 'sold_transaction', 'gain_loss', 'cash_onhand', 'position_value', 'total_position', 'accumu_return'])
+    portfolio.iloc[0]['trade_action'] = 0
+    portfolio.iloc[0]['qty_onhand'] = 0
+    portfolio.iloc[0]['cost_basis'] = 0.00
+    portfolio.iloc[0]['sold_transaction'] = 0.00
+    portfolio.iloc[0]['gain_loss'] = 0.00
+    portfolio.iloc[0]['cash_onhand'] = initial_capital
+    portfolio.iloc[0]['position_value'] = 0.00
+    portfolio.iloc[0]['total_position'] = initial_capital
+    portfolio.iloc[0]['accumu_return'] = portfolio.iloc[0]['total_position'] - initial_fund
+    recent_cost = 0
+
+    # The conditional parameters below determine how the trading strategy will be carried out
+    for i in range(1, len(portfolio)):
+        if (trading_model.iloc[i]['entry_exit'] == 1) and (portfolio.iloc[i-1]['qty_onhand'] == 0):
+            portfolio.iloc[i]['trade_action'] = 1
+            portfolio.iloc[i]['qty_onhand'] = portfolio.iloc[i-1]['qty_onhand'] + portfolio.iloc[i]['trade_action']
+            portfolio.iloc[i]['cost_basis'] = trading_model.iloc[i]['open_price'] * portfolio.iloc[i]['trade_action']
+            portfolio.iloc[i]['sold_transaction'] = 0.00
+            portfolio.iloc[i]['gain_loss'] = 0.00
+            portfolio.iloc[i]['cash_onhand'] = portfolio.iloc[i-1]['cash_onhand'] - portfolio.iloc[i]['cost_basis']
+            recent_cost = trading_model.iloc[i]['open_price'] * portfolio.iloc[i]['trade_action']
+            if verbose: print('BOUGHT QTY:', portfolio.iloc[i]['trade_action'], 'on', portfolio.index[i], 'at the price of', trading_model.iloc[i]['open_price'])
+        elif (trading_model.iloc[i]['entry_exit'] == -1) and (portfolio.iloc[i-1]['qty_onhand'] > 0):
+            portfolio.iloc[i]['trade_action'] = -1
+            portfolio.iloc[i]['qty_onhand'] = portfolio.iloc[i-1]['qty_onhand'] + portfolio.iloc[i]['trade_action']
+            portfolio.iloc[i]['cost_basis'] = 0.00
+            portfolio.iloc[i]['sold_transaction'] = trading_model.iloc[i]['open_price'] * portfolio.iloc[i]['trade_action'] * -1
+            portfolio.iloc[i]['gain_loss'] = (recent_cost + (trading_model.iloc[i]['open_price'] * portfolio.iloc[i]['trade_action'])) * -1
+            portfolio.iloc[i]['cash_onhand'] = portfolio.iloc[i-1]['cash_onhand'] + portfolio.iloc[i]['sold_transaction']
+            recent_cost = 0.00
+            if verbose: print('SOLD QTY:', portfolio.iloc[i]['trade_action'], 'on', portfolio.index[i], 'at the price of', trading_model.iloc[i]['open_price'])
+        else:
+            portfolio.iloc[i]['trade_action'] = 0
+            portfolio.iloc[i]['qty_onhand'] = portfolio.iloc[i-1]['qty_onhand']
+            portfolio.iloc[i]['cost_basis'] = portfolio.iloc[i-1]['cost_basis']
+            portfolio.iloc[i]['sold_transaction'] = 0.00
+            portfolio.iloc[i]['gain_loss'] = 0.00
+            portfolio.iloc[i]['cash_onhand'] = portfolio.iloc[i-1]['cash_onhand']
+        portfolio.iloc[i]['position_value'] = trading_model.iloc[i]['close_price'] * portfolio.iloc[i]['qty_onhand']
+        portfolio.iloc[i]['total_position'] = portfolio.iloc[i]['cash_onhand'] + portfolio.iloc[i]['position_value']
+        portfolio.iloc[i]['accumu_return'] = portfolio.iloc[i]['total_position'] - initial_fund
+
+    return portfolio
+
+
+# In[27]:
+
+
+portfolio_collection = {}
+
+# Build dataframe for reporting model performance summary
+performance_summary = pd.DataFrame(columns=['model_name','return_value','return_percent'])
+
+for key in model_collection:
+    portfolio_collection[key] = trading_portfolio_generation(initial_capital, model_collection[key])
+    if initial_capital != 0:
+        return_percentage = portfolio_collection[key].accumu_return[-1] / initial_capital * 100
+    else:
+        return_percentage = None
+    performance_summary = performance_summary.append({'model_name': key, 'return_value': portfolio_collection[key].accumu_return[-1],
+                                                      'return_percent': return_percentage}, ignore_index=True)
+print()
+
+
+# In[28]:
+
+
+# Display the model performance summary
+performance_summary.sort_values(by=['return_value'], inplace=True, ascending=False)
+print(performance_summary)
+print()
+
+
+# In[29]:
+
+
+# Display the transactions from the top model
+top_model = performance_summary.iloc[0]['model_name']
+print('The transactions from the top model %s:' % (top_model))
+print(portfolio_collection[top_model][portfolio_collection[top_model].trade_action != 0])
+print()
+
+
+# ## Task 5. Evaluate Performance
+
+# In[32]:
+
+
+# Calculate the stock's performance for a long-only model
+model_template = model_template[model_start_date:model_end_date]
+long_only_return = model_template.iloc[-1]['close_price'] - model_template.iloc[0]['open_price']
+print('The performance of the long-only model from day one is: $%.2f' %(long_only_return))
+print()
+
+
+# In[33]:
+
+
+best_model = ''
+best_return = 0
+for key in portfolio_collection:
+    if portfolio_collection[key]['accumu_return'][-1] > best_return:
+        best_model = key
+        best_return = portfolio_collection[best_model]['accumu_return'][-1]
+print('The best model found is:', best_model)
+print('The best profit/loss for the investing period is: $%.2f' % (best_return))
+if initial_capital != 0:
+    print('The best return percentage for initial capital is: %.2f%%' % (best_return / initial_capital * 100))
+print()
+
+
+# In[34]:
+
+
+worst_model = None
+worst_return = long_only_return
+for key in portfolio_collection:
+    if portfolio_collection[key]['accumu_return'][-1] < worst_return:
+        worst_model = key
+        worst_return = portfolio_collection[worst_model]['accumu_return'][-1]
+print('The worst model found is:', worst_model)
+print('The worst profit/loss for the investing period is: $%.2f' % (worst_return))
+if initial_capital != 0:
+    print('The worst return percentage for the initial capital is: %.2f%%' % (worst_return / initial_capital * 100))
+print()
+
+
+# In[36]:
+
+
+for key in model_collection:
+    print('Processing portfolio for model:', key)
+    trade_transactions = portfolio_collection[key][portfolio_collection[key].trade_action != 0]
+    print(trade_transactions)
+    print('Accumulated profit/loss for one share of stock with initial capital of $%.0f at the end of modeling period: $%.2f' % (initial_capital, portfolio_collection[key].accumu_return[-1]))
+    if initial_capital != 0:
+        print('Accumulated return percentage based on the initial capital investment: %.2f%%' % (return_percentage))
+    if trade_transactions.iloc[-1]['trade_action'] == 1:
+        print('The current status of the model is:', 'Holding a position since', trade_transactions.index.tolist()[-1], '\n')
+    else:
+        print('The current status of the model is:', 'Waiting to enter since', trade_transactions.index.tolist()[-1], '\n')
+
+
+# In[37]:
+
+
+sys.stdout = orig_stdout
+f.close()
+
+print ('Total time for the script:',(datetime.now() - startTimeScript))
+print ('The output was stored in the file:', filename)
