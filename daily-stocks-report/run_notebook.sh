3b40629d6ee5c237e290fd63a73851934b6ce808
David Lowe <david.lowe@dainesanalytics.com>
2020-07-01 10:42:54 -0700

    Upload #1 of the run_notebook shell script

---
+++ daily-stocks-report/run_notebook.sh
@@ -0,0 +1,2 @@
+/opt/conda/bin/jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_ema_crossover_grid_search_AAPL.ipynb
+/opt/conda/bin/jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_sma_crossover_grid_search_AAPL.ipynb
