c63aefb848d78a8830aaddfdb1a74ccd8c94b89c
David Lowe <david.lowe@dainesanalytics.com>
2020-11-20 17:11:02 -0800

    Upload #6 of run_notebook batch file

--- daily-stocks-report/run_notebook.bat
+++ daily-stocks-report/run_notebook.bat
@@ -12,20 +12,28 @@ jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algot
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_GOOGL.ipynb
 
-jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_JNJ.ipynb
-
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_MRVL.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_NFLX.ipynb
 
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_NOW.ipynb
+
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_NVDA.ipynb
 
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_OKTA.ipynb
+
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_PYPL.ipynb
+
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_REGN.ipynb
+
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_SQ.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_SWKS.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_TSLA.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_TSM.ipynb
+
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_TWLO.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute py_algotrading_combo_ma_crossover_grid_ZM.ipynb
